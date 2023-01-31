d042b24ba58b3d851377581f78c2fc6381281cec
witehound <netvid00@gmail.com>
2023-01-31 16:24:03 +0100

    addedd compoenent folder and haeder componennt

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Todo App</h1>
+      <nav>
+        <a
+          href="
+    "
+        >
+          Home
+        </a>
+        <a
+          href="
+    "
+        >
+          Status
+        </a>
+        <a
+          href="
+    "
+        >
+          About
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
