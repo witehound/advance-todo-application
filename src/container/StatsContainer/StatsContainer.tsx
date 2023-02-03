6c611b0c65e36e0190eff1ef906b537cf4e811f5
witehound <netvid00@gmail.com>
2023-02-03 13:11:37 +0100

    addedd stats container an styling

--- src/container/StatsContainer/StatsContainer.tsx
+++ src/container/StatsContainer/StatsContainer.tsx
@@ -1,25 +1,50 @@
+import { useEffect, useState } from "react";
 import { todoServices } from "../../service";
-import "./StatsContainer.module.css";
+import styles from "./StatsContainer.module.css";
+
+import { Todo } from "../../models/todo";
 
 type StatsContainerProps = {
   todoService: todoServices;
 };
 
-const StatsContainer = ({}: StatsContainerProps) => {
+const StatsContainer = ({ todoService }: StatsContainerProps) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const loadTodos = async () => {
+    const data = await todoService.getAllTodo();
+    setTodos(data);
+  };
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const doneTodos = todos && todos.filter((todo) => todo.isDone);
+  const activeTodos = todos && todos.filter((todo) => !todo.isDone);
+
   return (
-    <div>
-      <h2>Stats</h2>
-      <div>
-        <div>To do</div>
-        <ul>
-          <li>1</li>
-        </ul>
-      </div>
-      <div>
-        <div>Not do</div>
-        <ul>
-          <li>2</li>
-        </ul>
+    <div className={styles.container}>
+      <h2 className={styles.h2}>Stats</h2>
+      <div className={styles.stats}>
+        <div>
+          <div>Active To do : ({doneTodos.length})</div>
+          <ul className={styles.ul}>
+            {doneTodos.map((todo, i) => (
+              <li key={i} className={styles.li}>
+                {todo.task}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <div>Done To Do : ({activeTodos.length})</div>
+          <ul className={styles.ul}>
+            {activeTodos.map((todo, i) => (
+              <li key={i} className={styles.li}>
+                {todo.task}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
